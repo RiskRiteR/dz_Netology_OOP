611feaee1889b5125f5e3e4ce72d166525a5f28d
vladimir kazimir <kazimir.vm@gmail.com>
2023-03-06 17:20:50 +0300

    Task №3

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Student:
+    instance_list_s = []
+
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -6,6 +8,8 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.average_grades = 0
+        Student.instance_list_s.append(self)
 
     def rate_lectors(self, lector, course, grade):
         if isinstance(lector, Lecturer) and course in self.courses_in_progress and course in lector.courses_attached \
@@ -17,6 +21,36 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_rating(self):
+        if sum([len(i) for i in self.grades.values()]) > 0:
+            result = round(sum([sum(i) for i in self.grades.values()]) /
+                           sum([len(i) for i in self.grades.values()]), 2)
+            self.average_grades = result
+            return result
+
+    def completion_course(self, course):
+        self.finished_courses.append(course)
+        self.courses_in_progress.remove(course)
+
+    def __str__(self):
+        result = f'Имя: {self.name}\nФамилия: {self.surname}\n' \
+                 f'Средняя оценка за домашние задания: {self.average_grades}\n' \
+                 f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' \
+                 f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        return result
+
+    def __gt__(self, other):
+        if isinstance(other, Student):
+            return self.average_rating() > other.average_rating()
+        print('Ошибка')
+        return
+
+    def __eq__(self, other):
+        if isinstance(other, Student):
+            return self.average_rating() == other.average_rating()
+        print('Ошибка')
+        return
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -26,9 +60,37 @@ class Mentor:
 
 
 class Lecturer(Mentor):
+    instance_list_l = []
+
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades_lecturer = {}
+        self.average_grades_l = 0
+        Lecturer.instance_list_l.append(self)
+
+    def average_rating(self):
+        if sum([len(i) for i in self.grades_lecturer.values()]) > 0:
+            result = round(sum([sum(i) for i in self.grades_lecturer.values()]) /
+                           sum([len(i) for i in self.grades_lecturer.values()]), 2)
+            self.average_grades_l = result
+            return result
+
+    def __str__(self):
+        result = f'Имя: {self.name}\nФамилия: {self.surname}\n' \
+                 f'Средняя оценка за лекции: {self.average_grades_l}'
+        return result
+
+    def __gt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_rating() > other.average_rating()
+        print('Ошибка')
+        return
+
+    def __eq__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_rating() == other.average_rating()
+        print('Ошибка')
+        return
 
 
 class Reviewer(Mentor):
@@ -44,3 +106,7 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        result = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return result
