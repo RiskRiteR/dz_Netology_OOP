be50f75d16cad9c6de5cf78e94e4748ee6d24404
vladimir kazimir <kazimir.vm@gmail.com>
2023-03-06 10:39:37 +0300

    Task_№2

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lectors(self, lector, course, grade):
+        if isinstance(lector, Lecturer) and course in self.courses_in_progress and course in lector.courses_attached \
+                and 0 <= grade <= 10:
+            if course in lector.grades_lecturer:
+                lector.grades_lecturer[course] += [grade]
+            else:
+                lector.grades_lecturer[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -18,8 +28,19 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.grades_lecturer = {}
 
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress \
+                and 0 <= grade <= 10:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
